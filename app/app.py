49cecdabe81ae19f6a2d70eae5b0d834145a6100
mukkamallapradeep <mukkamallapradeep@gmail.com>
2026-01-16 16:11:40 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,7 +8,8 @@ def home():
 
 @app.route("/health")
 def health():
-    return jsonify({"status":"ok"})
+    return f"Hello health status ok. Created at {datetime.now()}"
+    # return jsonify({"status":"ok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
